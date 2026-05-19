139dfd25ec62bd4d68f67cee51d3cba7c0bb4087
Srihari Soundararajan <srihari@Sriharis-MacBook-Air.local>
2026-05-19 15:07:17 +0530

    adding nodes

---
+++ node.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+
+from react import llm, tools
+
+load_dotenv()
+
+system_message = """
+You are a helpful assistant that that can use tools to answer questions
+"""
+
+def run_agent_reasoning(state: MessagesState) -> MessagesState:
+    """A simple agent that uses the tools to answer questions."""
+    response = llm.invoke([{"role": "system", "content": system_message}, *state.messages])
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools=tools)
---
+++ react.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+import os
+
+load_dotenv()
+
+@tool
+def triple(num: float) -> float:
+    """Triples the input number."""
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
